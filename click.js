17936892d27865fae3d46dc56c4bc5ee78f4caec
bkmunoz <82331185+bkmunoz@users.noreply.github.com>
2021-04-30 23:58:08 -0500

    Initial Upload

---
+++ click.js
@@ -0,0 +1,11 @@
+function displayFact() {
+    alert("A cow-bison hybrid is called a “beefalo”.");
+}
+
+function displayDefinition() {
+    alert("Being the detective in a crime drama where you are also the murderer.");
+}
+
+function displayHidden(id) {
+    id.innerHTML = "Nice job!";
+}
